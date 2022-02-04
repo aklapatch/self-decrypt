b784b0088392d76ee50a50a756231f2ed7330adb
Aaron Klapatch <klapatchaaron@gmail.com>
2022-02-04 14:56:15 -0500

    Added button callback.

--- main.c
+++ main.c
@@ -4,6 +4,33 @@
 #include <iup.h>
 
 
+static char in_path[10240] = "", out_path[10240] = "";
+
+int get_out_path(Ihandle *self){
+  Ihandle *dlg = IupFileDlg();
+  IupSetAttributes(dlg, "DIALOGTYPE = SAVE, TITLE = \"Archive Save\"");
+  IupSetAttributes(dlg, "FILTER = \"*.exe\", FILTERINFO = \"Executable Files\"");
+  IupPopup(dlg, IUP_CENTER, IUP_CENTER);
+  switch(IupGetInt(dlg, "STATUS"))
+  {
+    case 1:
+      memset(out_path, 0, sizeof(out_path));
+      IupMessage("New file",IupGetAttribute(dlg, "VALUE"));
+      strncpy(out_path, IupGetAttribute(dlg, "VALUE"), sizeof(out_path));
+      printf("out_file = %s\n", out_path);
+      break;
+
+    case 0 :
+      IupMessage("File already exists",IupGetAttribute(dlg, "VALUE"));
+      break;
+
+    case -1 :
+      IupMessage("IupFileDlg","Operation Canceled");
+      return 1;
+      break;
+  }
+  return IUP_DEFAULT;
+}
 
 int main(int argc, char **argv)
 {
@@ -12,13 +39,14 @@ int main(int argc, char **argv)
   IupSetLanguage("ENGLISH");
   IupSetGlobal("UTF8MODE", "Yes");
 
-  char in_path[10240] = "", out_path[10240] = "";
 
   Ihandle *f_in_text = IupText(NULL), 
 	  *in_button = IupButton("Browse", "btn_f_in"),
 	  *f_out_text = IupText(NULL),
 	  *out_button = IupButton("Browse", "btn_f_out"),
 	  *next_button = IupButton("Next", "btn_next");
+
+  IupSetCallback( out_button, "ACTION", (Icallback)get_out_path);
   
   IupSetAttribute(f_in_text, "READONLY", "YES");
   IupSetAttribute(f_out_text, "READONLY", "YES");
@@ -38,6 +66,7 @@ int main(int argc, char **argv)
 		)
 	      );
   IupShowXY( dlg, IUP_CENTER, IUP_CENTER );
+  IupSetAttributes(dlg, "EXPAND = YES, TITLE = self-decrypt");
   IupMainLoop();
 
   /* Finishes IUP */
--- makefile
+++ makefile
@@ -1,3 +1,11 @@
+ifeq ($(OS), Windows_NT)
+	LIBS=iup/lib/mingw4/libiup.a -Iiup/include -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
+	ENV=MINGW4=/mingw64 TEC_UNAME=mingw4
+else
+	ENV=""
+	LIBS=iup/lib/linux510_64/libiup.a -Iiup/include/ -lgtk-3 -lgdk-3 -lglib-2.0 -lgobject-2.0 -lpango-1.0 -lpangocairo-1.0 -lcairo -lgdk_pixbuf-2.0 -lm -lx11
+endif
+
 concat: concat.c
 	$(CC) -Wall -Werror concat.c -o concat
 
@@ -6,23 +14,28 @@ concat-test: concat
 	hexdump -C test.out
 	cat test.log
 
+.PHONY: tomcrypt
 tomcrypt:
 	make -C libtomcrypt-1.18.2/
 
+.PHONY: iup
 iup:
-	make -C iup/
+	echo "building for "
+	echo $(OS)
+	$(ENV) make -C iup/src
+	#$(ENV) make -C iup/srcfiledlg/
 
 ui:
-	$(CC) ui_test.c  -o ui_test iup/lib/Linux510_64/libiup.a -Iiup/include/ -lgtk-3 -lgdk-3 -lglib-2.0 -lgobject-2.0 -lpango-1.0 -lpangocairo-1.0 -lcairo -lgdk_pixbuf-2.0 -lm -lX11
+	$(CC) ui_test.c  -o ui_test $(LIBS)
 
 run_ui: ui
 	./ui_test
 
 play:
-	$(CC) button_test.c  -o button_test iup/lib/Linux510_64/libiup.a -Iiup/include/ -lgtk-3 -lgdk-3 -lglib-2.0 -lgobject-2.0 -lpango-1.0 -lpangocairo-1.0 -lcairo -lgdk_pixbuf-2.0 -lm -lX11
+	$(CC) button_test.c  -o button_test $(LIBS)
 	./button_test
-main:
-	$(CC) main.c  -o self-decrypt iup/lib/Linux510_64/libiup.a -Iiup/include/ -lgtk-3 -lgdk-3 -lglib-2.0 -lgobject-2.0 -lpango-1.0 -lpangocairo-1.0 -lcairo -lgdk_pixbuf-2.0 -lm -lX11
+main: tomcrypt iup
+	$(CC) main.c  -o self-decrypt $(LIBS) 
 
 test: main
 	./self-decrypt
