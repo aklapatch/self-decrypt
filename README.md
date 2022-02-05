1e8875cd3169a95913e41297e465122c700cc4a2
Aaron Klapatch <klapatchaaron@gmail.com>
2022-02-04 22:06:20 -0500

    Progress

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # self-decrypt
 Hopefully this will be a program that makes self-decrypting archives
+
+## General idea
+I want to be able to send an encrypted payload that is password
+protected. The program should prompt for a password when cliced on, and
+then if the password is correct, then it will extract the payload. The
+payload should be AES-256 encrypted, and verified with ECC P384 if possible.
+
+### Crypto plans
+- Use Argon2id for password hashing.
+- Use tomcrypt for ChaCha20Poly1305 and ecc sign+verify
+
+First, ECC verify the password hash.
+The password hash will be split into a key, IV, and a MAC addition. 
+The whole password hash will also be used for authdata.
+There will be some user provided randomness (from the interval of how many times they push a ui button or something, fed into chacha20), that will be used to create an ECC private key.
+A paired public key will be made from that private key. That key pair
+will be used to sign the payload MAC. Part of the payload MAC will come
+from ChaCha20Poly1305, and the other 24 bytes will come from part of the password hash.
+
+### File Format Plans
+The self-decrypting archive format:
+```
+| decryption program |
+|--------------------|
+| encrypted payload  |
+|--------------------|
+| Payload signature  |
+|--------------------|
+| password signature |
+|--------------------|
+| ECC P384 Pub key   |
+|--------------------|
+| payload length u64 |
+|--------------------|
+| sentinel value     |
+```
+The decryption will open itself as a file, ask for a password, then try
+to decrypt the value.
+
+The encrypting program format:
+```
+| encryption program           |
+|------------------------------|
+| decryption program           |
+|------------------------------|
+| decrypt program length (u64) |
+|------------------------------|
+| sentinel value               |
+```
+The encryption program will append the encrypted payload to the
+decryption program.
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <iup.h>
@@ -6,8 +7,14 @@
 
 static char in_path[10240] = "", out_path[10240] = "";
 
-static Ihandle *f_in_text = NULL, *f_out_text = NULL; 
+static Ihandle *f_in_text = NULL, *f_out_text = NULL, *pw_text = NULL, *next_button = NULL; 
 
+static bool has_in_path = false, has_out_path = false;
+
+void maybe_activate_next_button(){
+  const char * attr = has_in_path && has_out_path ? "ACTIVE = YES" : "ACTIVE = NO";
+  IupSetAttributes(next_button, attr);
+}
 int get_in_path(Ihandle *self){
   Ihandle *dlg = IupFileDlg();
   IupSetAttributes(dlg, "DIALOGTYPE = OPEN, TITLE = \"File Select\"");
@@ -20,8 +27,10 @@ int get_in_path(Ihandle *self){
       memset(in_path, 0, sizeof(in_path));
       strncpy(in_path, IupGetAttribute(dlg, "VALUE"), sizeof(in_path));
       IupSetAttribute(f_in_text, "VALUE", in_path);
+      has_in_path = true;
       break;
   }
+  maybe_activate_next_button();
   return IUP_DEFAULT;
 }
 
@@ -34,39 +43,54 @@ int get_out_path(Ihandle *self){
   {
     case 1:
     case 0:
+      memset(in_path, 0, sizeof(in_path));
       strncpy(out_path, IupGetAttribute(dlg, "VALUE"), sizeof(out_path));
       IupSetAttribute(f_out_text, "VALUE", out_path);
-      IupSetAttributes(f_out_text, "READONLY = YES, EXPAND = YES, SIZE = 4x8");
-      IupRefresh(f_out_text);
+      has_out_path = true;
       break;
   }
+  // make the Generate file button active if we have both paths and a password.
+  maybe_activate_next_button();
+  return IUP_DEFAULT;
+}
+
+
+int encrypt_archive_cb(Ihandle *self){
+
   return IUP_DEFAULT;
 }
 
 int main(int argc, char **argv)
 {
-    Ihandle *filedlg;
   IupOpen(&argc, &argv);
   IupSetLanguage("ENGLISH");
   IupSetGlobal("UTF8MODE", "Yes");
 
   f_in_text = IupText(NULL);
   f_out_text = IupText(NULL);
+  pw_text = IupText(NULL);
+  next_button = IupButton("Generate File", "next_button");
+  IupSetAttributes(next_button, "ACTIVE = NO");
+
   Ihandle *in_button = IupButton("Browse", "btn_f_in"),
 	  *out_button = IupButton("Browse", "btn_f_out"),
-	  *next_button = IupButton("Next", "btn_next"),
-	  *in_label = IupLabel("Input File"),
-	  *out_label = IupLabel("Output File");
+	  *in_label = IupLabel(" Input File: "),
+	  *out_label = IupLabel(" Output File:"),
+	  *pw_label = IupLabel(" Password:");
 
-  IupSetAttributes(in_label, "PADDING = 3x3");
-  IupSetAttributes(out_label, "PADDING = 3x3");
+  IupSetAttributes(in_label, "PADDING = 5x5, SIZE = 60x8");
+  IupSetAttributes(out_label, "PADDING = 5x5, SIZE = 60x8");
+  IupSetAttributes(pw_label, "PADDING = 5x5, SIZE = 60x8");
   IupSetCallback( out_button, "ACTION", (Icallback)get_out_path);
 
   IupSetCallback( in_button, "ACTION", (Icallback)get_in_path);
   
-  IupSetAttributes(f_in_text, "READONLY = YES, EXPAND = YES, SIZE = 4x8");
-  IupSetAttribute(f_out_text, "READONLY", "YES");
-  IupSetAttribute(f_out_text, "EXPAND", "YES");
+  IupSetAttributes(f_in_text, "READONLY = YES, EXPAND = HORIZONTAL, SIZE = 200x, PADDING = 10x10");
+  IupSetAttributes(f_in_text, "VALUE = \"Use this Browse Button to select an input file. ->\"");
+
+  IupSetAttributes(f_out_text, "READONLY = YES, EXPAND = HORIZONTAL, PADDING = 10x10");
+  IupSetAttributes(f_out_text, "VALUE = \"Use this Browse Button to select an output file. ->\"");
+  IupSetAttributes(pw_text, "PASSWORD = YES, EXPAND = HORIZONTAL, PADDING = 10x10");
 
   Ihandle *dlg = IupDialog(
 	      IupVbox(
@@ -80,7 +104,11 @@ int main(int argc, char **argv)
 		  f_out_text,
 		  out_button,
 		  NULL),
-		next_button,
+		IupHbox(
+		  pw_label,
+		  pw_text,
+		  next_button,
+		  NULL),
 		NULL
 		)
 	      );
