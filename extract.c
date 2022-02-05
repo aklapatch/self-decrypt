497d676fd20eeb3f1c33be959e5c223b116602f9
Aaron Klapatch <klapatchaaron@gmail.com>
2022-02-05 17:59:50 -0500

    Almost ready for extraction testing.

--- extract.c
+++ extract.c
@@ -5,6 +5,53 @@
 #include <iup.h>
 #include "util.h"
 
+static Ihandle *pw_text= NULL, *decrypt_button = NULL;
+
+static char self_name[4096] = {0};
+
+int decrypt_cb(Ihandle *self){
+  // read the length of the archive
+
+  FILE *self = fopen(self_name, "rb");
+  if (!self){
+    IupMessagef("Error", "Failed Opening %s for extraction!", self_name);
+    return IUP_DEFAULT;
+  }
+
+  uint64_t file_size = 0;
+  if (fseek(self, - (SENTINEL_LEN + sizeof(file_size)), SEEK_END) != 0){
+	IupMessagef("Error", "Failed to seek in %s", argv[0]);
+	goto closein;
+  }
+
+  if (fread(&file_size, 1, sizeof(file_size), self) != sizeof(file_size)){
+	IupMessagef("Error", "Failed to read %s", argv[0]);
+	goto closein;
+  }
+
+  //seek to the beginning of the stored file (not the .exe file)
+  if (fseek(self, - (SENTINEL_LEN + sizeof(file_size) + file_size), SEEK_END) != 0){
+	IupMessagef("Error", "Failed to seek in %s", argv[0]);
+	goto closein;
+  }
+  
+  FILE *fout = fopen("decrypted.txt", "wb");
+  if (fout == NULL){
+	IupMessagef("Error", "Failed to open output file!");
+	goto closein;
+  }
+
+  // decrypt the achive and write it out
+  uint8_t file_buf[FREAD_BYTES] = {0};
+
+closeout:
+  fclose(fout);
+
+closein:
+  fclose(self);
+  return IUP_DEFAULT;
+}
+
 int main(int argc, char **argv){
   IupOpen(&argc, &argv);
   IupSetLanguage("ENGLISH");
@@ -18,6 +65,7 @@ int main(int argc, char **argv){
 	IupMessagef("Error", "Failed to open %s", argv[0]);
 	return EXIT_FAILURE;
   }
+
   // go to the end and see if a sentinel is present
   if (fseek(self, - SENTINEL_LEN, SEEK_END) != 0){
 	IupMessagef("Error", "Failed to get to the end of %s", argv[0]);
@@ -36,30 +84,32 @@ int main(int argc, char **argv){
 	IupMessagef("Error", "File sentinel not found %s\nIs file attached?", argv[0]);
 	goto cleanup;
   }
-
-  // read the length of the archive
-
-  uint64_t file_size = 0;
-  if (fseek(self, - (SENTINEL_LEN + sizeof(file_size)), SEEK_END) != 0){
-	IupMessagef("Error", "Failed to seek in %s", argv[0]);
-	goto cleanup;
-  }
-
-  if (fread(&file_size, 1, sizeof(file_size), self) != sizeof(file_size)){
-
-	IupMessagef("Error", "Failed to read %s", argv[0]);
-	goto cleanup;
-  }
-
-  //seek to the beginning of the stored file (not the .exe file)
-  if (fseek(self, - (SENTINEL_LEN + sizeof(file_size) + file_size), SEEK_END) != 0){
-	IupMessagef("Error", "Failed to seek in %s", argv[0]);
-	goto cleanup;
-  }
-  
-  // decrypt the achive
-
-cleanup:
+  // close the file for now. We'll open it if we need to later.
   fclose(self);
-  return rc;
+
+  // copy the path of the exe into this buffer so we can use it later.
+  strncpy(self_name, argv[0], sizeof(self_name) - 1);
+
+  // set up the window to ask for a password
+  pw_text = IupText(NULL);
+  decrypt_button = IupButton("Decrypt File", "decrypt_button");
+  IupSetCallback(decrypt_button, (Icallback)decrypt_cb);
+  Ihandle *pw_label = IupLabel("Enter Password:");
+
+  IupSetAttributes(pw_text, "EXPAND = HORIZONTAL, SIZE = 150x, PADDING = 10x10, PASSWORD = YES");
+
+  Ihandle *dlg = IupDialog(
+      IupHbox(
+	pw_label,
+	pw_text,
+	decrypt_button,
+	NULL),
+      NULL);
+  IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
+  IupSetAttributes(dlg, "EXPAND = YES, TITLE = extract encrypted file");
+  IupMainLoop();
+
+  IupClose();
+
+  return EXIT_SUCCESS;
 }
